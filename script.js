e89f7851de53ac445abbf1e1ff48b0914f7089bf
Luiz Carlos <luizcdev@outlook.com>
2024-08-26 07:32:19 -0300

    Projeto de um amigo

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Obrigado por entrar em contato! Você pode nos enviar um e-mail para gabriel@nutricionista.com.br ou ligar para +55 (21)98370-2226.');
+});
